1a0637095e66ba977602f3ea815210f3603590ae
samsho4log <mil2020@naver.com>
2026-03-10 21:17:13 +0900

    samsho4 Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+io.on("connection", (socket) => {
+
+  console.log("user connected");
+
+  socket.on("join", (nickname) => {
+    socket.nickname = nickname;
+    io.emit("chat", nickname + " joined");
+  });
+
+  socket.on("chat", (msg) => {
+    io.emit("chat", socket.nickname + ": " + msg);
+  });
+
+});
+
 server.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
