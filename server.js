440c7eb3f13e1262c43ed071642d5344b518ff7f
samsho4log <mil2020@naver.com>
2026-03-10 21:07:49 +0900

    samsho4 server.js

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
+const express = require("express");
 const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
-const HOST = "0.0.0.0";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.end("Samsho4 server running");
+app.use(express.static("public"));
+
+const users = {};
+
+io.on("connection", (socket) => {
+  console.log("user connected:", socket.id);
+
+  socket.on("join", (nickname) => {
+    users[socket.id] = {
+      nickname: nickname || "익명",
+    };
+
+    io.emit("userList", Object.values(users).map((u) => u.nickname));
+    io.emit("chatMessage", `${users[socket.id].nickname} 님이 입장했습니다.`);
+  });
+
+  socket.on("chatMessage", (msg) => {
+    if (!users[socket.id]) return;
+    io.emit("chatMessage", `${users[socket.id].nickname}: ${msg}`);
+  });
+
+  socket.on("disconnect", () => {
+    if (users[socket.id]) {
+      io.emit("chatMessage", `${users[socket.id].nickname} 님이 퇴장했습니다.`);
+      delete users[socket.id];
+      io.emit("userList", Object.values(users).map((u) => u.nickname));
+    }
+    console.log("user disconnected:", socket.id);
+  });
 });
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server running on ${HOST}:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
